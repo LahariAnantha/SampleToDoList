5f4e2b31e3adcd418196bd5dca656c0ec5c3dce8
Lahari Anantha <lahari.anantha@gmail.com>
2022-07-19 11:50:40 -0700

    Initial Commit

---
+++ SampleToDoList/ToDo.swift
@@ -0,0 +1,15 @@
+//
+//  ToDo.swift
+//  SampleToDoList
+//
+//  Created by Lahari Anantha on 7/19/22.
+//
+
+import UIKit
+
+class ToDo{
+    
+    var name = ""
+    
+    var important = false
+}
